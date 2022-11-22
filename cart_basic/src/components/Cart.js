e445d698eca057c68d0813736db9e3e2a08e57dc
nhattruong0811 <truong-tcn@if-vietnam.com>
2022-11-22 17:03:43 +0700

    up

---
+++ cart_basic/src/components/Cart.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import CartItem from './CartItem';
+import CartReasult from './CartReasult';
+
+
+class Cart extends Component {
+
+    render(){
+        return (
+            <div className="container">
+                <section className="section">
+                    <div className="table-responsive">
+                        <table className="table product-table">
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Sản Phẩm</th>
+                                    <th>Giá</th>
+                                    <th>Số Lượng</th>
+                                    <th>Tổng Cộng</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <CartItem />
+                                <CartItem />
+                                <CartItem />
+                               <CartReasult />
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </div>
+          );
+    }
+}
+  
+  export default Cart;
+  
---
+++ cart_basic/src/reducers/products.js
@@ -0,0 +1,35 @@
+var initialState = [
+    {
+        id : '1',
+        name: 'IP 7',
+        image: 'https://didongviet.vn/pub/media/catalog/product//i/p/iphone-7-vang-didongviet.jpg',
+        description: 'Sản Phẩm do apple sản xuất',
+        price: 500,
+        inventory : 10,
+        rating : 3
+    },
+    {
+        id : '2',
+        name: 'IP 8',
+        image: 'https://24hstore.vn/images/products/2020/04/19/large/iphone-8-red.jpg',
+        description: 'Sản Phẩm do apple sản xuất',
+        price: 550,
+        inventory : 10,
+        rating: 4
+    },
+    {
+        id : '1',
+        name: 'IP 14',
+        image: 'https://cdn.tgdd.vn/Products/Images/42/289694/iphone-14-pro-bac-thumb-600x600.jpg',
+        description: 'Sản Phẩm do apple sản xuất',
+        price: 600,
+        inventory : 10,
+        rating: 5
+    }
+];
+const product = (state = initialState, action) =>{
+    switch(action.type){
+        default : return [...state];
+    }
+}
+export default product
